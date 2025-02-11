7c8aaf710309985ccfaf278e2eafbff6d7dffc13
mrjk <mrjk.78@gmail.com>
2025-02-11 16:10:38 -0500

    poc: move _WorkingDir to WorkingDirNode in core

--- paasify_v4/core.py
+++ paasify_v4/core.py
@@ -14,12 +14,30 @@ with controlled initialization patterns.
 
 import os
 import logging
+from typing import List, Optional, Union
+from pathlib import Path
+import paasify_v4.exception as exc
 
 # pylint: disable=unused-import
 from pprint import pprint
-from paasify_v4.common import read_file, from_yaml
+
+
+from superconf.anchors2 import PathAnchor, FileAnchor
+
+
+from paasify_v4.common import (
+    read_file,
+    from_yaml,
+    find_file_up,
+    list_parent_dirs,
+    to_json,
+)
 
 logger = logging.getLogger(__name__)
+
+
+# Parent Node class
+# ================================================
 
 
 # pylint: disable=too-few-public-methods
@@ -90,6 +108,10 @@ def requires_setup_node(name="setup_node"):
         return wrapper
 
     return decorator
+
+
+# Paasify AppNode class
+# ================================================
 
 
 class HelperMethods:
@@ -286,3 +308,100 @@ class AppNode(HelperMethods, Node):
     def __bool__(self):
         "Check if node is empty"
         return True
+
+
+# Working Directory class
+# ================================================
+
+
+class WorkingDirNode(AppNode):
+    "Working directory mixin class"
+
+    # node__iterate_backend = "_children"
+    # node__iterate_setupmarker = "setup_node"
+
+    ALLOWED_CONF_FILES = []
+    OBJECT_NAME = "working_dir"
+
+    def __init__(self, ident=None, parent=None, path=None, search_up=None):
+        super().__init__(ident=ident, parent=parent)
+
+        _root_path, _config_file = self.find_workdir(path=path, search_up=search_up)
+
+        root_path = PathAnchor(_root_path, mode="rel")
+        root_config_path = FileAnchor(path=_config_file, parent=root_path)
+        self._path = root_path
+        self.config_path = root_config_path
+
+        logger.debug("Workdir %s config file: %s", self.OBJECT_NAME, ~root_config_path)
+        self.config = self.load_config(~root_config_path)
+
+        # TODO: Remove absolute path in logs
+        logger.info(
+            "Initilize %s from: %s", self.OBJECT_NAME, self._path.get_path(mode="abs")
+        )
+
+    def load_config(self, config: Optional[str] = None):
+        "Load the namespace config from a file"
+
+        _conf = ~self.config_path if not config else config
+        if os.path.isfile(_conf):
+            return from_yaml(read_file(_conf))
+
+        return {}
+
+    def find_workdir(self, path=None, search_up=None):
+        "Find the namespace path and config file"
+
+        # Prepare discovery method
+        # ------------------------
+        config_files = None
+        root_path = None
+        if search_up and path:
+            logger.debug("Workdir %s search up from: %s", self.OBJECT_NAME, path)
+            paths = list_parent_dirs(path)
+            config_files = find_file_up(self.ALLOWED_CONF_FILES, paths)
+        elif path:
+            if os.path.isfile(path):
+                logger.debug("Workdir %s file fetch from: %s", self.OBJECT_NAME, path)
+                config_files = [path]
+                root_path = os.path.dirname(config_files[0])
+            else:
+                logger.debug(
+                    "Workdir %s directory fetch from: %s", self.OBJECT_NAME, path
+                )
+                root_path = path
+                config_files = find_file_up(self.ALLOWED_CONF_FILES, [path])
+        else:
+            raise exc.PaasifyError(
+                f"No path or search_up provided to start {self.OBJECT_NAME}"
+            )
+
+        # Load configuration file
+        # ------------------------
+        # Actually, we should have a list of zero or one config file.
+        if len(config_files) > 1:
+            msg = f"Multiple config files found: {config_files}, we use only first one"
+            logger.warning(msg)
+
+        config_file = self.ALLOWED_CONF_FILES[0]
+        if len(config_files) > 0:
+            # The first item should always exists and be an existing file since we scanned
+            # the directory above.
+            config_file = config_files[0]
+            if not root_path:
+                root_path = os.path.dirname(config_file)
+
+        # Final checks
+        if not root_path:
+            target = self.ALLOWED_CONF_FILES[0]
+            msg = f"Can't find file '{target}' for {self.OBJECT_NAME} in path: {path}"
+            if search_up:
+                msg = f"Can't find file '{target}' for {self.OBJECT_NAME} in hierarchy of: {path}"
+
+            raise exc.PaasifyWorkdirNotFoundError(msg)
+        assert config_file
+
+        # Create anchored paths
+        # ------------------------
+        return (root_path, config_file)
--- paasify_v4/devel.py
+++ paasify_v4/devel.py
@@ -6,8 +6,6 @@ from pprint import pprint
 # from dataclasses import dataclass
 # from types import SimpleNamespace
 import os.path
-from pathlib import Path
-from typing import List, Optional, Union
 
 # from superconf.anchors import PathAnchor
 
@@ -18,118 +16,20 @@ from paasify_v4.common import (
     list_parent_dirs,
     to_json,
 )
-from paasify_v4.core import AppNode, setup_once, requires_setup_node
+from paasify_v4.core import AppNode, WorkingDirNode, setup_once, requires_setup_node
 import paasify_v4.exception as exc
 
-from superconf.anchors2 import PathAnchor, FileAnchor
 
 # from paasify_v4.catalog import PaasifyCatalog
 
 logger = logging.getLogger(__name__)
 
 
-# Directory Common classes
-# ================================================
-
-
-class _WorkingDir(AppNode):
-    "Working directory mixin class"
-
-    # node__iterate_backend = "_children"
-    # node__iterate_setupmarker = "setup_node"
-
-    ALLOWED_CONF_FILES = []
-    OBJECT_NAME = "working_dir"
-
-    def __init__(self, ident=None, parent=None, path=None, search_up=None):
-        super().__init__(ident=ident, parent=parent)
-
-        _root_path, _config_file = self.find_workdir(path=path, search_up=search_up)
-
-        root_path = PathAnchor(_root_path, mode="rel")
-        root_config_path = FileAnchor(path=_config_file, parent=root_path)
-        self._path = root_path
-        self.config_path = root_config_path
-
-        logger.debug("Workdir %s config file: %s", self.OBJECT_NAME, ~root_config_path)
-        self.config = self.load_config(~root_config_path)
-
-        # TODO: Remove absolute path in logs
-        logger.info(
-            "Initilize %s from: %s", self.OBJECT_NAME, self._path.get_path(mode="abs")
-        )
-
-    def load_config(self, config: Optional[str] = None):
-        "Load the namespace config from a file"
-
-        _conf = ~self.config_path if not config else config
-        if os.path.isfile(_conf):
-            return from_yaml(read_file(_conf))
-
-        return {}
-
-    def find_workdir(self, path=None, search_up=None):
-        "Find the namespace path and config file"
-
-        # Prepare discovery method
-        # ------------------------
-        config_files = None
-        root_path = None
-        if search_up and path:
-            logger.debug("Workdir %s search up from: %s", self.OBJECT_NAME, path)
-            paths = list_parent_dirs(path)
-            config_files = find_file_up(self.ALLOWED_CONF_FILES, paths)
-        elif path:
-            if os.path.isfile(path):
-                logger.debug("Workdir %s file fetch from: %s", self.OBJECT_NAME, path)
-                config_files = [path]
-                root_path = os.path.dirname(config_files[0])
-            else:
-                logger.debug(
-                    "Workdir %s directory fetch from: %s", self.OBJECT_NAME, path
-                )
-                root_path = path
-                config_files = find_file_up(self.ALLOWED_CONF_FILES, [path])
-        else:
-            raise exc.PaasifyError(
-                f"No path or search_up provided to start {self.OBJECT_NAME}"
-            )
-
-        # Load configuration file
-        # ------------------------
-        # Actually, we should have a list of zero or one config file.
-        if len(config_files) > 1:
-            msg = f"Multiple config files found: {config_files}, we use only first one"
-            logger.warning(msg)
-
-        config_file = self.ALLOWED_CONF_FILES[0]
-        if len(config_files) > 0:
-            # The first item should always exists and be an existing file since we scanned
-            # the directory above.
-            config_file = config_files[0]
-            if not root_path:
-                root_path = os.path.dirname(config_file)
-
-        # Final checks
-        if not root_path:
-            target = self.ALLOWED_CONF_FILES[0]
-            msg = f"Can't find file '{target}' for {self.OBJECT_NAME} in path: {path}"
-            if search_up:
-                msg = f"Can't find file '{target}' for {self.OBJECT_NAME} in hierarchy of: {path}"
-
-            raise exc.PaasifyWorkdirNotFoundError(msg)
-        assert config_file
-
-        # Create anchored paths
-        # ------------------------
-        return (root_path, config_file)
-
-
 # Pod classes
 # ================================================
 
 
-class PaasifyPod(_WorkingDir):
+class PaasifyPod(WorkingDirNode):
     "Base class for all Paasify pods"
 
     OBJECT_NAME = "Pod"
@@ -143,7 +43,7 @@ class PaasifyPod(_WorkingDir):
 # ================================================
 
 
-class PaasifyStack(_WorkingDir):
+class PaasifyStack(WorkingDirNode):
     "Base class for all Paasify stacks"
 
     OBJECT_NAME = "Stack"
@@ -204,7 +104,7 @@ class PaasifyNoNamespace(AppNode):
         super().__init__(**kwargs)
 
 
-class PaasifyNamespace(_WorkingDir):
+class PaasifyNamespace(WorkingDirNode):
     "Namespace class, manage list of stacks"
 
     # node__iterate_backend = "_children"
